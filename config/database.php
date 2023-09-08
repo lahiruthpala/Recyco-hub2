ed3e1b2c69655b5b746189fdae179527821123b6
lahiruthpala <lahiruthpala@gmail.com>
2023-09-08 14:14:36 +0530

    Create the file structure
    Have to edit the configuration files .env file

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'your_database_name',
+    'username'  => 'your_database_user',
+    'password'  => 'your_database_password',
+    'charset'   => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix'    => '',
+];
