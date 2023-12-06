0b4c7961d0e9cf498c09b9515c22a39e68f647cb
lahiruthpala <lahiruthpala@gmail.com>
2023-12-06 14:39:48 +0530

    Collector Side Bar

--- private/controllers/Collector.php
+++ private/controllers/Collector.php
@@ -2,15 +2,13 @@
 class collector extends Controller
 {
 
-	function index()
-	{
-		// code...
+    function index()
+    {
         $pickup = $this->load_model('PickUpRequestModel');
-        // Auth::getCollector_ID
         $pickup = $pickup->where("Collector_ID", "25435");
-        
-        $this->view('Collector/collector',  ['rows'=>$pickup]);
-	}
+
+        $this->view('Collector/collector', ['rows' => $pickup]);
+    }
 
 
     function table()
@@ -23,54 +21,19 @@ class collector extends Controller
     function details($id)
     {
         $user = $this->load_model('PickUpRequestModel');
-		$data = $user->first('pickupId', $id);
-        $this->view('Collector/new', ['pickup'=>$data]);
+        $data = $user->first('pickupId', $id);
+        $this->view('Collector/new', ['pickup' => $data]);
     }
     public function inventory()
     {
         $pickup = $this->load_model('RawInvnetoryModel');
         $pickup = $pickup->where("collectorId", "25435");
-       
+
         $this->view('Collector/inventory', ['rows' => $pickup]);
     }
-    
-  
-    
-    function declination(){
-        $this->view('declination');
-    }
 
-	function pendingpickups(){
-		$inventory = $this->load_model('PickUpRequestModel');
-		$data = $inventory->where('Status', 'Pending');
-		$this->view('Colletor/PendingRequestTable', ['rows'=>$data]);
-	}
 
-    function jobs($id, $type){
-        $pickup = $this->load_model('PickUpRequestModel');
-        $completionDate = $_POST['completion_date'] ?? date('Y-m-d');
-        // Auth::getCollector_ID
-        $arr = [];
-        $arr['Status'] = $type.'ed';
-        $arr['Completeddate'] = $completionDate;
-        $data = $pickup->Update($id, $arr, "pickupId");
-        $this->index();
-    }
-    function inven($id, $type){
-        $in = $this->load_model('RawInvnetoryModel');
-       
-        $arr = [];
-        $arr['Inventorystatus'] = $type;
-      
-        $data = $in->Update($id, $arr, "InventoryId");
-        $this->inventory();
-        $data = $user->first('pickupId', $id);
-        $this->view('pickup details', ['pickup' => $data]);
-    }
-    function confirmation()
-    {
-        $this->view('confirmation');
-    }
+
     function declination()
     {
         $this->view('declination');
@@ -81,5 +44,34 @@ class collector extends Controller
         $inventory = $this->load_model('PickUpRequestModel');
         $data = $inventory->where('Status', 'Pending');
         $this->view('Colletor/PendingRequestTable', ['rows' => $data]);
+    }
+
+    function jobs($id, $type)
+    {
+        $pickup = $this->load_model('PickUpRequestModel');
+        $completionDate = $_POST['completion_date'] ?? date('Y-m-d');
+        // Auth::getCollector_ID
+        $arr = [];
+        $arr['Status'] = $type . 'ed';
+        $arr['Completeddate'] = $completionDate;
+        $data = $pickup->Update($id, $arr, "pickupId");
+        $this->index();
+    }
+    function inven($id, $type)
+    {
+        $in = $this->load_model('RawInvnetoryModel');
+
+        $arr = [];
+        $arr['Inventorystatus'] = $type;
+
+        $data = $in->Update($id, $arr, "InventoryId");
+        $this->inventory();
+        $data = $in->first('pickupId', $id);
+        $this->view('pickup details', ['pickup' => $data]);
+    }
+
+    function confirmation()
+    {
+        $this->view('confirmation');
     }
 }
