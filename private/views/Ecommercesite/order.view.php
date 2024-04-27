6683698744bbf20c268f5fbcfe22015b10793515
sithuminiashch <136596859+sithuminiashch@users.noreply.github.com>
2024-04-27 08:47:01 +0530

    product details view quantity submit

--- private/views/Ecommercesite/order.view.php
+++ private/views/Ecommercesite/order.view.php
@@ -1,9 +1,6 @@
 <?php
 $this->view('include/head');
-
-$quantity = 0;
 ?>
-
 <body>
     <div class="layout js-layout layout--fixed-drawer layout--fixed-header is-small-screen">
         <?php $this->view('include/header') ?>
@@ -22,37 +19,44 @@ $quantity = 0;
                                     <form class="form form--basic" action="<?= ROOT ?>/Ecommercesite/order/<?= $row->product_Id ?>" method="POST">
                                         <div class="grid">
                                             <div class="cell cell--6-col-desktop cell--6-col-tablet cell--1-col-phone">
-                                                <!-- Image -->
+                                              
                                                 <img src="<?=ROOT?>/images/ecommercesite_images/<?= strtolower($row->product_name) ?>.jpg" alt="Image" style="width: 400px; height: 300px; margin-top: 40px; margin-left: 40px;">
                                             </div>
                                             <div class="cell cell--6-col-desktop cell--6-col-tablet cell--1-col-phone" style="margin-top: 60px; margin-left: 230px;">
-                                                <!-- Product Name -->
+                                               
                                                 <div style="display: flex; align-items: center;">
                                                     <label for="product-name" style="margin-right: 80px; color:black; margin-left: 10px; font-size:20px"><p style="margin-right:5px;">Product</p></label>
-                                                    <input type="text" id="product-name" name="product-name" value="<?= $row->product_name ?>" readonly>
+                                                    <input type="text" id="product-name" name="product-name" style="font-size:15px" value="<?= $row->product_name ?>" readonly>
                                                 </div>
 
-                                                <!-- Price -->
+                                              
                                                 <div style="display: flex; align-items: center;">
-                                                    <label for="price" style="margin-right: 80px; color:black; margin-left: 10px; font-size:20px;"><p style="margin-right:30px;">Price</p></label>
-                                                    <input type="text" id="price" name="price" value="<?= $row->price ?>" readonly>
+                                                    <label for="price" style="margin-right: 30px; color:black; margin-left: 10px; font-size:20px;"><p style="margin-right:30px;">Price(1Kg)</p></label>
+                                                    <input type="text" id="price" name="price"  style="font-size:15px" value="<?= $row->price ?>" readonly>
                                                 </div>
 
-                                                <!-- Discount -->
+                                              <?php if($row->discount!=Null): ?>
                                                 <div style="display: flex; align-items: center;">
                                                     <label for="discount" style="margin-right: 78px; color:black; margin-left: 10px; font-size:20px"><p>Discount</p></label>
-                                                    <input type="text" id="discount" name="discount" value="<?= $row->discount ?>%" readonly>
+                                                    <input type="text" id="discount" name="discount"  style="font-size:15px" value="<?= $row->discount ?>%" readonly>
                                                 </div>
-
-                                                <!-- Available Quantity -->
+                                               <?php else: ?>
                                                 <div style="display: flex; align-items: center;">
-                                                    <label for="quantity" style="margin-right: 76px; color:black; margin-left: 10px; font-size:20px"><p>Quantity</p></label>
-                                                    <input type="number" id="quantity" name="quantity" value="" >
+                                                    <label for="discount" style="margin-right: 78px; color:black; margin-left: 10px; font-size:20px"><p>Discount</p></label>
+                                                    <input type="text" id="discount" name="discount"  style="font-size:15px" value="0%" readonly>
+                                                </div>   
+                                                <?php endif; ?>
+
+                                             
+                                                <div style="display: flex; align-items: center;">
+                                                       <label for="quantity" style="margin-right: 50px; color:black; margin-left: 10px; font-size:20px"><p>Quantity(kg)</p></label>
+                                                       <input type="number" id="quantity" name="quantity"  style="font-size:15px" value="" required min="1">
                                                 </div>
 
-                                                <!-- Submit Button -->
+
+                                               
                                                 <div style="margin-top: 30px;">
-                                                    <button type="submit" style="padding: 10px 20px; background-color: #139571; color: white; border: none;font-size:15px;">Place Order</button>
+                                                    <button type="submit" style="padding: 10px 20px; background-color: #139571; color: white; border: none;font-size:20px;">Place Order</button>
                                                 </div>
                                             </div>
                                         </div>
@@ -61,7 +65,7 @@ $quantity = 0;
                             </div>
                         <?php endforeach; ?>
                     <?php else: ?>
-                        <p>No pickup jobs available.</p>
+                        <p>product not available.</p>
                     <?php endif; ?>
                 </div>
             </div>
