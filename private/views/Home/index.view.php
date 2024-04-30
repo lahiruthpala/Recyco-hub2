ed879111be17649dcb53a69181ccc79c3f4dcdf9
lahiruthpala <lahiruthpala@gmail.com>
2024-04-30 09:12:44 +0530

    notification bug fix

--- private/views/Home/index.view.php
+++ private/views/Home/index.view.php
@@ -11,6 +11,7 @@
     <!-- Responsive -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
     <link href="css/responsive.css" rel="stylesheet">
 
 </head>
